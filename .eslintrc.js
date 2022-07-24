599b415f1320c984b0c383ba78eebfeedebbbec8
MinatoHikari <35342316+MinatoHikari@users.noreply.github.com>
2022-07-24 23:17:02 +0800

    eslint rule

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,7 +4,6 @@ module.exports = {
         '@typescript-eslint/indent': 'off',
         '@typescript-eslint/semi': 'off',
         '@typescript-eslint/member-delimiter-style': 'off',
-        semi: 'off',
         'arrow-parens': 'off',
         'vue/html-indent': ['warn', 4],
         'vue/html-self-closing': 'off',
